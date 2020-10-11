cec35243eca9966af4f592bf73329e0cf1f58672
badal-cloud <aryar6563@gmail.com>
2020-10-10 17:00:38 -0700

    Add new Alarm tone and add Live Clock

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let timerObj = {
 
 function soundAlarm() {
   let amount = 3;
-  let audio = new Audio("Timer_Sound_Effect.mp3");
+  let audio = new Audio("13767_morning_alarm.mp3");
   function playSound() {
     audio.pause();
     audio.currentTime = 0;
